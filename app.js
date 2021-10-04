e8e7b950ca87de8d5800c0ba59c5d3119ba3c09f
Teresa Nguyen <88606876+teresaanguyen@users.noreply.github.com>
2021-10-04 11:56:16 -0700

    implemented handling user events

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-// TODO
+var GroceryListItem = (props) => {
+
+  var onListItemClick = (event) => {
+    console.log('I got clicked');
+  };
+
+  return (
+    <ul>
+      <li onClick={onListItemClick}>{props.groceryItems[0]}</li>
+      <li>{props.groceryItems[1]}</li>
+    </ul>
+  );
+}
+
+var GroceryList = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryListItem groceryItems={['Milk', 'Eggs']}/>
+  </div>
+)
+
+
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
