58e6bd01068263b3ea30a2797e39608f582437eb
Teresa Nguyen <88606876+teresaanguyen@users.noreply.github.com>
2021-10-04 12:27:03 -0700

    implemented list strikeout

--- app.js
+++ app.js
@@ -2,42 +2,35 @@ class GroceryListItemItem extends React.Component {
   constructor(props) {
     super(props);
 
-    // this.state = {
-    //   done: false
-    // };
+    this.state = {
+      done: false
+    };
   }
 
-  // onListItemClick() {
-  //   this.setState({
-  //     done: !this.state.done
-  //   });
-  // }
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
 
   render() {
-    // var style = {
-    //   textDecoration: this.state.done ? 'line-through' : 'none'
-    // };
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
 
     return (
-      //<li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.groceryItems}</li>
-      <li>{this.props.groceryItems}</li>
+      <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.groceryItems}</li>
     )
   }
 }
 
 var GroceryListItem = (props) => (
-
-  // var onListItemClick = (event) => {
-  //   console.log('I got clicked');
-  // };
-
-  //return (
     <ul>
       {props.groceryItems.map(groceryItems =>
       <GroceryListItemItem groceryItems={groceryItems} />
       )}
     </ul>
- // );
+
 )
 
 var GroceryList = () => (
